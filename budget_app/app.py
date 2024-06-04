a9f98a772e1429825ae970216de8a087a2cade3c
ritaly <malgorzata.lyczywek@gmail.com>
2024-06-04 18:17:29 +0200

    feat: move budget to module

--- budget_app/app.py
+++ budget_app/app.py
@@ -1,11 +1,9 @@
-from flask import Flask, flash, render_template, request, redirect, url_for
+from flask import Flask
 
 from controllers.budget import budget_bp
-from models.budget_manager import BudgetManger, InvalidExpenseError, NotFoundExpenseError
 
 app = Flask(__name__)
 app.secret_key = 'secret-key'
-budget_manager = BudgetManger()
 
 app.register_blueprint(budget_bp)
 
@@ -14,56 +12,6 @@ app.register_blueprint(budget_bp)
 @app.route('/index')
 def home():
     return 'Hello hello!'
-
-
-@app.route('/add', methods=['GET', 'POST'])
-def create_expense():
-    if request.method == 'POST':
-        description = request.form['description']
-        amount = float(request.form['amount'])
-        date = request.form['date']
-        try:
-            budget_manager.add_expense(description, amount, date)
-            flash("Successfully created expense", "success")
-            return redirect(url_for('home'))
-        except InvalidExpenseError as err:
-            flash(f"{err}", "danger")
-
-    return render_template('add.html')
-
-
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def update_expense(id):
-    if request.method == 'GET':
-        try:
-            expense = budget_manager.get_expense(id)
-            return render_template('edit.html', expense=expense)
-        except NotFoundExpenseError as err:
-            flash(f"{err}", "danger")
-            return redirect(url_for('home'))
-
-    if request.method == 'POST':
-        description = request.form['description']
-        amount = float(request.form['amount'])
-        date = request.form['date']
-        try:
-            budget_manager.update_expense(id, description, amount, date)
-            flash("Successfully edited expense", "success")
-            return redirect(url_for('home'))
-        except InvalidExpenseError as err:
-            flash(f"{err}", "danger")
-            expense = budget_manager.get_expense(id)
-            return render_template('edit.html', expense=expense)
-
-
-@app.route('/delete/<int:id>', methods=['POST'])
-def delete_expense(id):
-    try:
-        budget_manager.delete_expense(id)
-        flash("Successfully deleted expense", "success")
-    except InvalidExpenseError as err:
-        flash(f"{err}", "danger")
-    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
--- budget_app/controllers/budget/views.py
+++ budget_app/controllers/budget/views.py
@@ -1,20 +1,61 @@
-from flask import render_template
+from flask import Flask, flash, render_template, request, redirect, url_for
 
 from controllers.budget import budget_bp
-from models.budget_manager import BudgetManger
+from models.budget_manager import BudgetManger, InvalidExpenseError, NotFoundExpenseError
 
 budget_manager = BudgetManger()
 
 @budget_bp.route('/')
-def home():
-    return render_template('index.html', expenses=budget_manager.expenses)
+def show_expenses():
+    return render_template('budget.html', expenses=budget_manager.expenses)
 
 
-# create_expence
+@budget_bp.route('/add', methods=['GET', 'POST'])
+def create_expense():
+    if request.method == 'POST':
+        description = request.form['description']
+        amount = float(request.form['amount'])
+        date = request.form['date']
+        try:
+            budget_manager.add_expense(description, amount, date)
+            flash("Successfully created expense", "success")
+            return redirect(url_for('budget.home'))
+        except InvalidExpenseError as err:
+            flash(f"{err}", "danger")
 
-# read expence (single expence view)
+    return render_template('add.html')
 
-# update_expence
 
-# delete expence
+@budget_bp.route('/edit/<int:id>', methods=['GET', 'POST'])
+def update_expense(id):
+    if request.method == 'GET':
+        try:
+            expense = budget_manager.get_expense(id)
+            return render_template('edit.html', expense=expense)
+        except NotFoundExpenseError as err:
+            flash(f"{err}", "danger")
+            return redirect(url_for('show_expenses'))
+
+    if request.method == 'POST':
+        description = request.form['description']
+        amount = float(request.form['amount'])
+        date = request.form['date']
+        try:
+            budget_manager.update_expense(id, description, amount, date)
+            flash("Successfully edited expense", "success")
+            return redirect(url_for('home'))
+        except InvalidExpenseError as err:
+            flash(f"{err}", "danger")
+            expense = budget_manager.get_expense(id)
+            return render_template('edit.html', expense=expense)
+
+
+@budget_bp.route('/delete/<int:id>', methods=['POST'])
+def delete_expense(id):
+    try:
+        budget_manager.delete_expense(id)
+        flash("Successfully deleted expense", "success")
+    except InvalidExpenseError as err:
+        flash(f"{err}", "danger")
+    return redirect(url_for('show_expenses'))
 

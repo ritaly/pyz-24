3c01b7423eed60a7c055b06f44d3858f65720178
ritaly <malgorzata.lyczywek@gmail.com>
2024-06-13 19:28:33 +0200

    working with exchange rate

--- exchange_app/app/routes/product_routes.py
+++ exchange_app/app/routes/product_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request, redirect, url_for
 from ..repositories.product_repository import ProductRepository
+from ...config import Config
 
 product_bp = Blueprint('products', __name__)
 
@@ -15,6 +16,8 @@ def create_product():
     name = request.form['name']
     price_usd = request.form['price_usd']
     source = request.form['source']
+    api_key = Config.EX_RATE_API_KEY
+    base_url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/PLN/USD'
     price_pln = price_usd * 4
     ProductRepository.create_product(name, price_usd, price_pln, source)
     return redirect(url_for('home'))

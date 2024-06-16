9cc7b837b9cefd24b67a28e972b0d39c5c167ef6
ritaly <malgorzata.lyczywek@gmail.com>
2024-06-16 22:10:46 +0200

    routes - update & delete

--- exchange_app/app/repositories/product_repository.py
+++ exchange_app/app/repositories/product_repository.py
@@ -1,47 +1,41 @@
-from ..database import db
 from ..models.product import Product
-
+import pandas as pd
 
 class ProductRepository:
-    @staticmethod
-    def get_all_products():
+    def __init__(self, db):
+        self.db = db
+
+    def get_all_products(self):
         return Product.query.all()
 
-
-    @staticmethod
-    def create_product(name, price_usd, price_pln, source):
+    def create_product(self, name, price_usd, price_pln, source):
         if not name or not price_usd:
             return None
         new_product = Product(name=name, price_usd=price_usd, price_pln=price_pln, source=source)
-        db.session.add(new_product)
-        db.session.commit()
+        self.db.session.add(new_product)
+        self.db.session.commit()
+        return new_product
 
-
-    def update_product(self, product_id, name, price_usd, price_pln, last_update, source):
-        if not name or not price_usd:
-            return None
+    def update_product(self, product_id, name, price_usd, price_pln, last_updated, source):
         product = self.get_product(product_id)
+        if not product:
+            return None
         product.name = name
         product.price_usd = price_usd
         product.price_pln = price_pln
-        product.last_updated = last_update
+        product.last_updated = last_updated
         product.source = source
-        db.session.commit()
-
+        self.db.session.commit()
+        return product
 
     def delete_product(self, product_id):
         product = self.get_product(product_id)
         if not product:
             return None
-        db.session.delete(product)
-        db.session.commit()
-
+        self.db.session.delete(product)
+        self.db.session.commit()
 
     @staticmethod
     def get_product(product_id):
         return Product.query.get(product_id)
 
-
-    # def load_product(self, ):
-    ###
-    # load products from csv
--- exchange_app/app/routes/product_routes.py
+++ exchange_app/app/routes/product_routes.py
@@ -1,14 +1,17 @@
 from flask import Blueprint, request, redirect, url_for, jsonify
 
+from ..database import db
 from ..repositories.exchange_rate_repository import convert_to_pln
 from ..repositories.product_repository import ProductRepository
 
 product_bp = Blueprint('products', __name__)
 
+product_repo = ProductRepository(db)
+
 
 @product_bp.route('/', methods=['GET'])
 def list_products():
-    products = ProductRepository.get_all_products()
+    products = product_repo.get_all_products()
     return jsonify([product.to_dict() for product in products]) # list comprehention same as for p in products...
 
 
@@ -20,13 +23,19 @@ def create_product():
     source = request.form['source']
     price_pln = convert_to_pln(price_usd)
 
-    ProductRepository.create_product(name, price_usd, price_pln, source)
+    product_repo.create_product(name, price_usd, price_pln, source)
     return redirect(url_for('static_pages.products'))
 
-# HOMEWORK
-# update
+@product_bp.route('/products/<int:product_id>', methods=['PUT'])
+def update_product_route(product_id):
+    data = request.json
+    product_repo.update_product(product_id, data['name'], data['price_usd'])
+    return jsonify({'message': 'Product updated successfully'})
 
-# delete
+@product_bp.route('/products/<int:product_id>', methods=['DELETE'])
+def delete_product_route(product_id):
+    product_repo.delete_product(product_id)
+    return jsonify({'message': 'Product deleted successfully'})
 
 
 

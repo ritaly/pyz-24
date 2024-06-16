d1ea9d9c44a427259104adb7c825dec40b640b2a
ritaly <malgorzata.lyczywek@gmail.com>
2024-06-16 23:20:54 +0200

    improve url store

--- exchange_app/app/repositories/exchange_rate_repository.py
+++ exchange_app/app/repositories/exchange_rate_repository.py
@@ -5,8 +5,10 @@ from flask import current_app
 
 def convert_to_pln(price_usd):
     api_key = current_app.config['EX_RATE_API_KEY']
-    base_url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/USD/PLN'
-    response = requests.get(base_url)
+    base_url = current_app.config['EX_RATE_BASE_URL']
+    url = f'{base_url}/{api_key}/pair/USD/PLN'
+
+    response = requests.get(url)
     if response.status_code == 200:
         exchange_data = response.json()
         rate_usd_pln = exchange_data.get('conversion_rate')
--- exchange_app/config.py
+++ exchange_app/config.py
@@ -3,3 +3,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your_secret_key'
     EX_RATE_API_KEY = '73e7ed75084bd6a819952125'
+    EX_RATE_BASE_URL = 'https://v6.exchangerate-api.com/v6'
